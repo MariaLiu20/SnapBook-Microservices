e9c7b5ef197d6ef90eb94bf18e380733dd3d15ae
Maria Liu <47699128+MariaLiu20@users.noreply.github.com>
2022-11-22 18:30:27 -0500

    Changed event interfaces to types

--- moderator/index.js
+++ moderator/index.js
@@ -9,9 +9,9 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 };
 /**
  * Listen for CommentCreated events.
- * When this event is received, scan the comment for banned words.
+ * When CommentCreated event is received, scan the content for banned words.
  * Generate a CommentModerated event which includes original comment info received from CommentCreated event and
- * extend it with a status property, which indicates if the commentw as accepted or rejected
+ * extend it with a status property, which indicates if the comment was accepted or rejected
  */
 import express from 'express';
 import logger from 'morgan';
@@ -22,33 +22,28 @@ const port = 4003;
 // Middleware
 app.use(logger('dev'));
 app.use(express.json()); // parses HTTP req body into JSON object
-// const isCommentCreated = (e: any): e is CommentCreated =>
-//     e.type === "CommentCreated" && ("data.id" in e) && ("data.content" in e) && ("data.postId" in e);
-//     //typeof(e.data.id) == "string" && typeof(e.data.content == "string") && typeof(e.data.postId) == "string";
 app.post('/events', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    if (req.body.type == "CommentCreated") {
+    if (req.body.type == 'CommentCreated') {
         const { id, content, postId } = req.body.data;
         const bannedWords = ['donald', 'trump', 'republican', 'rightist', 'crap'];
-        let status = "accepted";
+        let status = 'accepted';
         let words = content.split(' ');
         words.forEach(word => {
             if (bannedWords.includes(word.toLowerCase())) {
-                status = "rejected";
+                status = 'rejected';
             }
         });
-        // Send post req to event bus with type and data {}
-        yield axios.post('http://eventbus:4005/events', {
-            // TODO: make 'PostCreated' an object instead of a string
-            // put in another .js file
-            // define smth in there that represents a postcreated type and import it
+        // Send post req to event bus with event type and data {}
+        let commentModerated = {
             type: 'CommentModerated',
             data: {
                 id,
                 content,
                 postId,
                 status
-            },
-        });
+            }
+        };
+        yield axios.post('http://eventbus:4005/events', commentModerated);
     }
     res.send({ status: 'OK' });
 }));
--- moderator/index.ts
+++ moderator/index.ts
@@ -1,77 +1,80 @@
 /**
  * Listen for CommentCreated events. 
- * When this event is received, scan the comment for banned words.
+ * When CommentCreated event is received, scan the content for banned words.
  * Generate a CommentModerated event which includes original comment info received from CommentCreated event and
- * extend it with a status property, which indicates if the commentw as accepted or rejected
+ * extend it with a status property, which indicates if the comment was accepted or rejected
  */
- import express, {Express, Request, Response} from 'express';
- import logger from 'morgan';
- //import cors from 'cors';
- import axios from 'axios';
- 
- const app: Express = express();
- const port: number = 4003;
- 
- // Middleware
- app.use(logger('dev'));
- app.use(express.json());            // parses HTTP req body into JSON object
- //app.use(cors());
- 
- // Interface to define event types
- interface Event {
-     type: "PostCreated" | "CommentCreated" | "CommentModerated" | "CommentVoted", 
-     data: {}
- }
- interface CommentCreated extends Event {
-     type: "CommentCreated",
-     data: {
-         id: string, 
-         content: string,
-         postId: string
-     }
- }
- interface TypedRequestBody<T> extends Express.Request {
-     body: T
- }
- // const isCommentCreated = (e: any): e is CommentCreated =>
- //     e.type === "CommentCreated" && ("data.id" in e) && ("data.content" in e) && ("data.postId" in e);
- //     //typeof(e.data.id) == "string" && typeof(e.data.content == "string") && typeof(e.data.postId) == "string";
- 
- app.post('/events', async (req: TypedRequestBody<Event>, res) => {
-     if (req.body.type == "CommentCreated") {
-         const { id, content, postId } = (req.body as CommentCreated).data;
-         const bannedWords = ['donald', 'trump', 'republican', 'rightist', 'crap'];
-         let status = "accepted";
-         let words = content.split(' ');
-         words.forEach(word => {
-             if (bannedWords.includes(word.toLowerCase())) {
-                 status = "rejected";
-             }
-         });
- 
-         // Send post req to event bus with type and data {}
-         await axios.post('http://eventbus:4005/events', {
-             // TODO: make 'PostCreated' an object instead of a string
-             // put in another .js file
-             // define smth in there that represents a postcreated type and import it
-             type: 'CommentModerated',                    
-             data: { 
-                 id,
-                 content,
-                 postId,
-                 status
-             },
-         });
-     }
- 
-     res.send({ status: 'OK' });
- });
- 
- app.post('/events', (req, res) => {
-     console.log(req.body.type);
-     res.send({});
-   });
-   
- app.listen(port, () => {
-     console.log('Listening on 4003');
-   });
+import express, { Express, Request, Response } from 'express';
+import logger from 'morgan';
+//import cors from 'cors';
+import axios from 'axios';
+
+const app: Express = express();
+const port: number = 4003;
+
+// Middleware
+app.use(logger('dev'));
+app.use(express.json());            // parses HTTP req body into JSON object
+//app.use(cors());
+
+// Interfaces/types to define events
+interface TypedRequestBody<T> extends Express.Request {
+    body: T
+}
+interface Event {
+    type: "PostCreated" | "CommentCreated" | "CommentModerated" | "CommentVoted",
+    data: {}
+}
+type CommentCreated = {
+    type: "CommentCreated",
+    data: {
+        id: string,
+        content: string,
+        postId: string
+    }
+} & Event;
+type CommentModerated = {
+    type: "CommentModerated",
+    data: {
+        id: string,
+        content: string,
+        postId: string,
+        status: string
+    }
+} & Event
+
+app.post('/events', async (req: TypedRequestBody<Event>, res: Response) => {
+    if (req.body.type == 'CommentCreated') {
+        const { id, content, postId } = (req.body as CommentCreated).data;
+
+        const bannedWords = ['donald', 'trump', 'republican', 'rightist', 'crap'];
+        let status = 'accepted';
+        let words = content.split(' ');
+        words.forEach(word => {
+            if (bannedWords.includes(word.toLowerCase())) {
+                status = 'rejected';
+            }
+        });
+        // Send post req to event bus with event type and data {}
+        let commentModerated: CommentModerated = {
+            type: 'CommentModerated',
+            data: {
+                id,
+                content,
+                postId,
+                status
+            }
+        };
+        await axios.post('http://eventbus:4005/events', commentModerated);
+    }
+    res.send({ status: 'OK' });
+});
+
+app.post('/events', (req, res) => {
+    console.log(req.body.type);
+    res.send({});
+});
+
+app.listen(port, () => {
+    console.log('Listening on 4003');
+});
